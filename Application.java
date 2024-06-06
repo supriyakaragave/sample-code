408e2cfceaff25d880d64200c04de16314606ec3
supriyakaragave <35910040+supriyakaragave@users.noreply.github.com>
2024-06-06 16:57:31 +0530

    Create Application.java

---
+++ Application.java
@@ -0,0 +1,30 @@
+package test.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@SpringBootApplication
+public class Application {
+
+
+    public static void main(String[] args) {
+       // SpringApplication.run(VehicleInformationUpdateApplication.class,args);
+
+
+        final ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+
+        final AtomicInteger counter = new AtomicInteger(0);
+
+        Arrays.asList(context.getBeanDefinitionNames())
+                .forEach(beanName -> {
+                    System.out.println("{}) Bean Name: {} "+ counter.incrementAndGet() + "\t" + beanName);
+                });
+
+    }
+
+
+}
